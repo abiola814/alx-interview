7386bf637cf4c53d0e9b7be191e621ef39125374
abiola oladunjoye <abiola.oladunjoye@tm30.net>
2023-07-27 00:35:41 +0100

    interview

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
-"""
-0. Pascal's Triangle
-"""
+"""Pascal Triangle Interview Challenge for Alx project conducted by Dr Marcus"""
 
 
 def pascal_triangle(n):
-    """Create a function def pascal_triangle(n): that returns a list of lists
-    of integers representing the Pascal’s triangle of n
-    """
-    res = []
-    if n > 0:
-        for i in range(1, n + 1):
-            level = []
-            C = 1
-            for j in range(1, i + 1):
-                level.append(C)
-                C = C * (i - j) // j
-            res.append(level)
-    return res
+    """returns a list of lists of numbers
+    representing the pascal triangle"""
+    if n <= 0:
+        return []
+
+    pascal_triangle = [0] * n
+
+    for i in range(n):
+        # This code snipet define and explained that  a row is  fill first and last idx with 1
+        row = [0] * (i+1)
+        row[0] = 1
+        row[len(row) - 1] = 1
+
+        for j in range(1, i):
+            if j > 0 and j < len(row):
+                x = pascal_triangle[i - 1][j]
+                y = pascal_triangle[i - 1][j - 1]
+                row[j] = x + y
+
+        pascal_triangle[i] = row
+
+    return pascal_triangle
